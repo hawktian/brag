ec0956ed3cb05f99997d29e437f6c8de94d5e3e8
Tonvin Tian <itonvin@gmail.com>
2022-12-02 12:59:07 +0000

    abandon nginx proxy

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-var addr = flag.String("addr", ":8080", "")
+var addr = flag.String("addr", ":8081", "")
 
 func main() {
 	flag.Parse()
@@ -17,8 +17,21 @@ func main() {
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
 	})
+	http.HandleFunc("/", index)
+	http.HandleFunc("/chat.html", chat)
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
 	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	http.ServeFile(w, r, "index.html")
+}
+
+func chat(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	http.ServeFile(w, r, "chat.html")
 }
