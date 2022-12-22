2db447a36d1dfbcf75d20b39201f2170188a508b
Tonvin Tian <itonvin@gmail.com>
2022-12-22 10:51:05 +0000

    use nginx as web proxy,go server only listen websocket request

--- main.go
+++ main.go
@@ -16,9 +16,11 @@ func main() {
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
 	})
-	http.HandleFunc("/", index)
-	http.HandleFunc("/chat.html", chat)
-	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
+
+	// http.HandleFunc("/", index)
+	// http.HandleFunc("/chat.html", chat)
+	// http.HandleFunc("/gate.html", gate)
+	// http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./dist/assets"))))
 
 	addr := conf.Server.Host + ":" + conf.Server.Port
 	if conf.Ssl.Status == "ON" {
@@ -29,12 +31,17 @@ func main() {
 	log.Println("Server started on " + addr)
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
-	http.ServeFile(w, r, "index.html")
-}
-
-func chat(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
-	http.ServeFile(w, r, "chat.html")
-}
+// func index(w http.ResponseWriter, r *http.Request) {
+// 	log.Println(r.URL)
+// 	http.ServeFile(w, r, "./dist/index.html")
+// }
+//
+// func chat(w http.ResponseWriter, r *http.Request) {
+// 	log.Println(r.URL)
+// 	http.ServeFile(w, r, "./dist/chat.html")
+// }
+//
+// func gate(w http.ResponseWriter, r *http.Request) {
+// 	log.Println(r.URL)
+// 	http.ServeFile(w, r, "./dist/gate.html")
+// }
