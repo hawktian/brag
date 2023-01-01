601db9baef827c7c8ccc6f8eec4d928bdfcda9cf
Tonvin Tian <itonvin@gmail.com>
2023-01-01 21:36:18 +0800

    delete server host config

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    host: "103.29.71.173",
-  },
   plugins: [vue()],
   root: "src",
   build: {
